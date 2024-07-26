51e19f250072f6822403499852e36a736b9a89d5
Gresi-Patel <patelgresi11@gmail.com>
2024-07-26 22:15:35 +0530

    added css and change image path

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,39 +2,51 @@ import React from "react";
 import SearchIcon from '@mui/icons-material/Search';
 import '../components/Home.css'
 // import hero_bg from '../Images/hero_bg.jpg'
-import home_bg from '../images/home_bg.jpg'
+// import home_bg from '../images/home_bg.jpg';
 
 const Home = () => {
 
-    return(
+    return (
         <div>
-                <div className="header">
-                    <div>
-                        <h2>MedInsight</h2>
+            <div className="header">
+                <div className="logo">
+                    <h2>MedInsight</h2>
+                </div>
+
+                <div className="searchBtn">
+                    {/* <SearchIcon /> */}
+                    <input type="text" placeholder="search by medicine name" name="search" />
+                    <div className="icon">
+                        <SearchIcon />
                     </div>
 
-                    <div>
-                        <nav>Home</nav>
-                        <nav>About</nav>
-                        <nav>Contact</nav>
-                    </div>
+                </div>
 
-                    <div>
+                <div className="navbar">
+                    <nav>Home</nav>
+                    <nav>About</nav>
+                    <nav>Contact</nav>
+                </div>
+
+                {/* <div className="searchBtn">
                     <SearchIcon />
                     <input type="text" placeholder="search" name="search" />
-                </div>
+                </div> */}
+            </div>
+
+            <div className="background" >
+                <div className="content">
+                    <b style={{ fontSize: '56px', color: 'green' }}>MedInsight</b>
+                    <div style={{ color: 'black',fontSize:"18px",fontWeight:"bold" }}>
+                        A platform where you will be getting all the information <br />
+                        regarding the medicines with it's uses.
+                    </div>
                 </div>
 
-<div className="background">
-        <b style={{fontSize:'56px',color:'green'}}>MedInsight</b>
-        <div style={{color:'black'}}>
-            A platform where you will be getting all the information <br />
-            regarding the medicines with it's uses.
-        </div>
-</div>
-               
+            </div>
+
         </div>
     )
 }
 
-export default Home
+export default Home;
